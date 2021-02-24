a12f9dc65ebdcc6aac6746c7ee5499d875bb3ac7
madenney <matt.a.denney@gmail.com>
2021-02-24 08:37:50 -0700

    basic skeleton completed

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ const app = express()
 const port = 3000
 
 const apiKeys = require('./.config/apiKeys')
-console.log(apiKeys)
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', async (req, res) => {
-  const data = await getData();
-  res.render('index', {data})
+  const trak4Data = await getTrak4Data()
+  const googleData = await getGoogleData(trak4Data)
+  res.render('index', {data:{trak4Data,googleData}})
 })
-
-//app.use(express.static('views'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-function getData(){
+function getTrak4Data(){
   console.log("getting data")
   return new Promise((resolve,reject) => {
 
@@ -41,8 +39,28 @@ function getData(){
         console.log("no logs found... :(")
         reject("no logs found... :(")
       }
-      resolve(data.data[0])
+      console.log(data.data)
+      resolve(data.data[data.data.length-269])
     });
   })
 
+}
+
+function getGoogleData(trak4Data){
+  return new Promise((resolve,reject) => {
+    let query = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
+    query += trak4Data.latitude + ','
+    query += trak4Data.longitude
+    query += '&key=AIzaSyBXVrtJ0syZ3iGnVBaoVj4PJPFsc7HJbPQ'
+
+    fetch(query,{
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log("google Data: ", data)
+      resolve(data.results[0])
+    })
+  })
 }
--- views/script.js
+++ views/script.js
@@ -1,10 +1,43 @@
 
 
 const main = function(){
-   console.log("hey")
-   $('#title').click(() => {
-      console.log('click')
-   })
+   console.log("data: ", data)
+   const addressBox = $('#addressBox')
+   const updatedBox = $('#updatedBox')
+
+   if(data.trak4Data.GPS_speed !== 0 ){
+      const { trak4Data, googleData } = data;
+      const { GPS_speed, GPS_heading } = trak4Data
+
+      console.log(GPS_speed,GPS_heading)
+      // add a check for weird GPS errors
+      $('#speed').html(GPS_speed > 80 || GPS_speed < -1 ? 70 : GPS_speed)
+
+      let direction = 'N/A'
+      const compass =
+         ['North','Northeast','East','Southeast',
+         'South','Southwest','West','Northwest','North']
+      let count = 22.5
+      for(var i = 0; i < 9; i++){
+         if(GPS_heading < count){
+            direction = compass[i]
+            break
+         } else {
+            count += 45
+         }
+      }
+      $('#direction').html(direction)
+
+      let county = googleData.address_components.find(a => {
+         return a.types.indexOf('administrative_area_level_2') > -1
+      }).long_name
+
+      let state = googleData.address_components.find(a => {
+         return a.types.indexOf('administrative_area_level_1') > -1
+      }).long_name
+      $('#movingLocation').html(`${county}, ${state}`)
+
+   }
 }
 
 
