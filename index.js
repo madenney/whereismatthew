44a3deee208f2440bd53c2493b0b105a9e0517fd
madenney <matt.a.denney@gmail.com>
2021-02-27 14:03:56 -0700

    version 1.0 complete

--- index.js
+++ index.js
@@ -2,65 +2,102 @@ const express = require('express')
 const fetch = require('node-fetch')
 const app = express()
 const port = 3000
+const fs = require('fs')
 
 const apiKeys = require('./.config/apiKeys')
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs')
+app.set('trust proxy', true)
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', async (req, res) => {
-  const trak4Data = await getTrak4Data()
-  const googleData = await getGoogleData(trak4Data)
-  res.render('index', {data:{trak4Data,googleData}})
+  try {
+    const trak4Data = await getTrak4Data()
+    const googleData = await getGoogleData(trak4Data)
+    res.render('index', {data:{trak4Data,googleData}, googleAPIKey: apiKeys.googleAPIKey})
+    log(req, 'success')
+  } catch(e){
+    console.log(e)
+    res.render('error')
+    log(req, 'error')
+  }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at port: ${port}`)
 })
 
+
 function getTrak4Data(){
-  console.log("getting data")
   return new Promise((resolve,reject) => {
 
-    fetch('https://gps.trak-4.com/api/v2/',{
-      method: 'POST',
-      body: JSON.stringify({
-        "commandstring": "get_reports_single_device",
-        "identifier": "015058000115437",
-        "datetime_start": "10/21/2020 00:00:00",
-        "datetime_end": "10/21/2021 00:00:00",
-        "coredataonly": true,
-        "token": apiKeys.trak4Key
-    }),
-      headers: { 'Content-Type': 'application/json' }
-    })
-    .then(response => response.json())
-    .then(data => {
-      if(!data.data[0]){
-        console.log("no logs found... :(")
-        reject("no logs found... :(")
-      }
-      console.log(data.data)
-      resolve(data.data[data.data.length-269])
-    });
-  })
+    const now = new Date()
+    const yesterday = new Date(now.getTime() - (86400000*2) )
+    now.setTime(now.getTime() + 86400000)
 
+    try {
+      fetch('https://gps.trak-4.com/api/v2/',{
+          method: 'POST',
+          body: JSON.stringify({
+            "commandstring": "get_reports_single_device",
+            "identifier": "015058000115437",
+            "datetime_start":  dateToTrak4Date(yesterday),
+            "datetime_end": dateToTrak4Date(now),
+            "coredataonly": true,
+            "token": apiKeys.trak4Key
+        }),
+          headers: { 'Content-Type': 'application/json' }
+        })
+        .then(response => response.json())
+        .then(data => {
+          if(!data.data || !data.data[0]){
+            return reject("no logs found...", data)
+          }
+          resolve(data.data[0])
+        });
+      } catch (e) {
+        reject(e)
+      }
+  })
 }
 
 function getGoogleData(trak4Data){
   return new Promise((resolve,reject) => {
-    let query = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
-    query += trak4Data.latitude + ','
-    query += trak4Data.longitude
-    query += '&key=AIzaSyBXVrtJ0syZ3iGnVBaoVj4PJPFsc7HJbPQ'
+    try {
+        let query = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
+        query += trak4Data.latitude + ','
+        query += trak4Data.longitude
+        query += `&key=${apiKeys.googleAPIKey}`
+        fetch(query,{
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' }
+        })
+        .then(response => response.json())
+        .then(data => {
+          if(!data.results){
+            console.log('something went wrong with google fetch:', data)
+            return reject(data)
+          }
+          resolve(data.results[0])
+        })
+    } catch(e){
+      reject('google fetch error')
+    }
+      })
+}
 
-    fetch(query,{
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log("google Data: ", data)
-      resolve(data.results[0])
-    })
+function dateToTrak4Date(d){
+  return`${("0" + (d.getMonth() + 1)).slice(-2)}/` +
+    `${("0" + (d.getDate())).slice(-2)}/` +
+    `${d.getFullYear()} ` +
+    `${("0" + (d.getHours())).slice(-2)}:` +
+    `${("0" + (d.getMinutes())).slice(-2)}:` +
+    `${("0" + (d.getSeconds())).slice(-2)}`
+}
+
+function log(req, msg){
+  const d = new Date()
+  let line = `${req.ip},${d.getTime()},${d.toTimeString()}\n`
+  fs.appendFile('access_log.csv', line, function (err) {
+    if (err) throw err;
   })
 }
